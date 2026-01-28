1aaadb8778318e4b09ed0a92576d08722ab7a340
User-N-prime <nilam@s.sfusd.edu>
2026-01-28 17:13:37 +0000

    i did it good

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -1,12 +1,15 @@
 package org.APCSLowell;
 
 public class Palindrome {
-    public boolean palindrome(String s) {
-        return false;
+    public boolean palindrome(String sWord) {
+        String s = sWord.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        return (reverse(s).equals(s));
     }
-    public String reverse(String s) {
-        String n = new String();
-        // your code here
-        return n;
+    public String reverse(String sWord) {
+        String s = "";
+        for (int i = sWord.length() - 1; i >= 0; i--) {
+            s += sWord.charAt(i);
+        }
+        return s;
     }
 }
